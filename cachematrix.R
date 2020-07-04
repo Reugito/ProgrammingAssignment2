fa601583ebdc2b280ea30201c1d8777e742a3f40
Reugito <65726495+Reugito@users.noreply.github.com>
2020-07-04 19:51:24 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,14 @@
+makeCacheMatrix <- function(x = matrix()){
+      inv <- NULL
+      set <- function(y){
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() {x}
+      setInverse <- function(inverse) {inv <<- inverse}
+      getInverse <- function() {inv}
+      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
 
 cacheSolve <- function(x, ...){
       inv <- x$getInverse()
@@ -10,4 +21,3 @@ cacheSolve <- function(x, ...){
       x$setInverse(inv)
       inv
 }
-
